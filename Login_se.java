01398ec0b3d0063409195b357b27dd1955f39074
SeEun <gse2881@gmail.com>
2023-02-21 17:21:02 +0900

    seeun2 first commit

---
+++ Login_se.java
@@ -0,0 +1,13 @@
+package Project01;
+
+public class Login_se {
+
+	public void login(String id, String pw) {
+
+		System.out.println("로그인 기능");
+		System.out.println("로그인 성공!");
+		//test
+
+	}
+
+}
